ab4d85833003ab320ecc978b0243922018433996
javipani12 <javier.fernandez.paniagua.22@gmail.com>
2022-11-16 23:37:44 +0100

    Tercera página equipo hecha

--- lib/screens/equipo_raimon_screen.dart
+++ lib/screens/equipo_raimon_screen.dart
@@ -6,10 +6,57 @@ class RaimonScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('RaimonScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Instituto Raimon'),
+        backgroundColor: Colors.orange,
       ),
+      body: Column(
+        children: const <Widget> [
+            SizedBox(height: 20,),
+            FotoRaimon(),
+            SizedBox(height: 20,),
+            TextoRaimon(),
+        ],
+      ),
+      backgroundColor: Colors.orangeAccent
+    );
+  }
+}
+
+class FotoRaimon extends StatelessWidget {
+
+  const FotoRaimon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const [
+        Expanded(
+          child: Image(
+            image: NetworkImage("https://static.wikia.nocookie.net/inazuma-eleven/images/4/40/Raimon_SD.png/revision/latest?cb=20220703151255"),
+            height: 100,
+            width: 100,
+            alignment: Alignment.bottomCenter,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextoRaimon extends StatelessWidget {
+
+  const TextoRaimon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const [
+        Text('El Instituto Raimon destaca por su coraje y garra'),
+      ],
     );
   }
 }
