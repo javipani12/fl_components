30dd7d010138a3761fffe19e8385763db94a0952
javipani12 <javier.fernandez.paniagua.22@gmail.com>
2022-11-16 23:12:23 +0100

    Primera página de equipo hecha

--- lib/screens/equipo_farm_screen.dart
+++ lib/screens/equipo_farm_screen.dart
@@ -6,10 +6,60 @@ class FarmScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('FarmScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Instituto Farm'),
+        backgroundColor: Colors.green,
       ),
+      body: Column(
+        children: const <Widget> [
+            SizedBox(height: 20,),
+            Row1(),
+            SizedBox(height: 20,),
+            Row2()
+        ],
+      ),
+      backgroundColor: Colors.greenAccent,
+    );
+  }
+}
+
+class Row1 extends StatelessWidget {
+
+  const Row1({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const [
+        Expanded(
+          child: Image(
+            image: NetworkImage('https://static.wikia.nocookie.net/inazuma-eleven/images/7/7c/Senbayama_SD.png'),
+            height: 100,
+            width: 100,
+            alignment: Alignment.bottomCenter,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Row2 extends StatelessWidget {
+
+  const Row2({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const [
+        Text('El Instituto Farm destaca por su capacidad defensiva'),
+      ],
     );
   }
 }
