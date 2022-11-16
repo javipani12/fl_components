1a950acfe3ebf62e2e6acb70da4e5d0c4c584b24
javipani12 <javier.fernandez.paniagua.22@gmail.com>
2022-11-16 23:46:45 +0100

    Quinto equipo hecho

--- lib/screens/equipo_zeus_screen.dart
+++ lib/screens/equipo_zeus_screen.dart
@@ -6,10 +6,57 @@ class ZeusScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('ZeusScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Instituto Zeus'),
+        backgroundColor: const Color.fromARGB(255, 9, 136, 13),
       ),
+      body: Column(
+        children: const <Widget> [
+            SizedBox(height: 20,),
+            FotoZeus(),
+            SizedBox(height: 20,),
+            TextoZeus(),
+        ],
+      ),
+      backgroundColor: const Color.fromARGB(255, 228, 225, 225)
+    );
+  }
+}
+
+class FotoZeus extends StatelessWidget {
+
+  const FotoZeus({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const [
+        Expanded(
+          child: Image(
+            image: NetworkImage("http://pm1.narvii.com/6486/df1a7278b8ed630281eb4b5ec04d94e2d8a0c70d_00.jpg"),
+            height: 100,
+            width: 100,
+            alignment: Alignment.bottomCenter,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TextoZeus extends StatelessWidget {
+
+  const TextoZeus({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: const [
+        Text('Se consideran los "Dioses" del fútbol'),
+      ],
     );
   }
 }
